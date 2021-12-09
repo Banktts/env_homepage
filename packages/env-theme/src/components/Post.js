22640e5c8145c1c5cff5df8a3b2bd8b596088389
thanabodee thamasroch <bankthamasaroch@gmail.com>
2021-12-10 02:44:07 +0700

    feat publication element in htmlProcessing not clean code yet

--- packages/env-theme/src/components/Post.js
+++ packages/env-theme/src/components/Post.js
@@ -1,18 +1,15 @@
-import {htmlProcessing} from "../helpers";
+import HtmlProcessing from "../helpers/htmlProcessing";
 import {connect} from "frontity";
 import {View} from "./View";
 
 const Post = ({state, actions, libraries}) => {
-    const Html2React = libraries.html2react.Component;
     const dataState = state.source.get(state.router.link)
     const postData = state.source.post[dataState.id].content.rendered
-    const postRender = htmlProcessing({htmlText: postData})
     console.log("dataState post:", dataState, "post data :", postData)
     return (
         <View>
             <div>
-                <Html2React
-                    html={postRender}/>
+                <HtmlProcessing htmlText={postData}   />
             </div>
         </View>
 
--- packages/env-theme/src/helpers/htmlProcessing.js
+++ packages/env-theme/src/helpers/htmlProcessing.js
@@ -1,3 +1,8 @@
+import {useState} from "react";
+import {connect} from "frontity";
+import {splitArray} from "./index";
+
+
 
 const mediaText = ({htmlText}) => {
     return htmlText.replaceAll("<div class=\"wp-block-media-text alignwide is-stacked-on-mobile\">", "<div class=\"columns is-desktop\">").replaceAll("<figure class=\"wp-block-media-text__media\">", "<div class=\"column\"> <figure class=\"image is-square\">").replaceAll("</figure>", "</figure></div>").replaceAll("<div class=\"wp-block-media-text__content\">", "<div class=\"column\">")
@@ -13,11 +18,11 @@ const title = ({htmlText}) => {
 }
 
 const titleBreak = ({htmlText}) => {
-    return htmlText.replaceAll("</h1>","</h1><br/>").replaceAll("</h2>","</h2><br/>").replaceAll("</h3>","</h3><br/>").replaceAll("</h4>","</h4><br/>").replaceAll("</h5>","</h5><br/>").replaceAll("</h6>","</h6><br/>")
+    return htmlText.replaceAll("</h1>", "</h1><br/>").replaceAll("</h2>", "</h2><br/>").replaceAll("</h3>", "</h3><br/>").replaceAll("</h4>", "</h4><br/>").replaceAll("</h5>", "</h5><br/>").replaceAll("</h6>", "</h6><br/>")
 }
 
 const address = ({htmlText}) => {
-    return htmlText.replaceAll("<p><strong>Address</strong></p>","<span class=\"icon-text\">\n" +
+    return htmlText.replaceAll("<p><strong>Address</strong></p>", "<span class=\"icon-text\">\n" +
         "  <span class=\"icon\">\n" +
         "    <i class=\"fas fa-address-book\"></i>\n" +
         "  </span>\n" +
@@ -26,7 +31,7 @@ const address = ({htmlText}) => {
 }
 
 const mail = ({htmlText}) => {
-    return htmlText.replaceAll("<p><strong>Mail</strong></p>","<span class=\"icon-text\">\n" +
+    return htmlText.replaceAll("<p><strong>Mail</strong></p>", "<span class=\"icon-text\">\n" +
         "  <span class=\"icon\">\n" +
         "    <i class=\"far fa-envelope\"></i>\n" +
         "  </span>\n" +
@@ -35,7 +40,7 @@ const mail = ({htmlText}) => {
 }
 
 const tel = ({htmlText}) => {
-    return htmlText.replaceAll("<p><strong>Tel</strong></p>","<span class=\"icon-text\">\n" +
+    return htmlText.replaceAll("<p><strong>Tel</strong></p>", "<span class=\"icon-text\">\n" +
         "  <span class=\"icon\">\n" +
         "    <i class=\"fas fa-phone-alt\"></i>\n" +
         "  </span>\n" +
@@ -44,7 +49,7 @@ const tel = ({htmlText}) => {
 }
 
 const fax = ({htmlText}) => {
-    return htmlText.replaceAll("<p><strong>Fax</strong></p>","<span class=\"icon-text\">\n" +
+    return htmlText.replaceAll("<p><strong>Fax</strong></p>", "<span class=\"icon-text\">\n" +
         "  <span class=\"icon\">\n" +
         "    <i class=\"fas fa-fax\"></i>\n" +
         "  </span>\n" +
@@ -53,7 +58,7 @@ const fax = ({htmlText}) => {
 }
 
 const resume = ({htmlText}) => {
-    return htmlText.replaceAll("<p><strong>Resume</strong></p>","<span class=\"icon-text\">\n" +
+    return htmlText.replaceAll("<p><strong>Resume</strong></p>", "<span class=\"icon-text\">\n" +
         "  <span class=\"icon\">\n" +
         "    <i class=\"far fa-id-badge\"></i>\n" +
         "  </span>\n" +
@@ -61,23 +66,96 @@ const resume = ({htmlText}) => {
         "</span>")
 }
 
+const singleTableWithTabsRender = ({val}) => {
+    console.log("table render process")
+    const N = Math.ceil(val.length / 5)
+    val = val.map((val) => {
+        return val + "<hr/>"
+    })
+    const [page, setPage] = useState(0)
+    const dataSplice = splitArray(val,5)
+
+    const [content, setContent] = useState(dataSplice[0])
+    console.log("dataSplice :",dataSplice)
+    console.log("initial content:",content)
+    const PageKey = [...Array(N).keys()].map((index) => {
+        return index === 0 ? "NEW" : `PAGE ${index + 1}`
+    })
+
+
+    const handlePage = ({key}) => {
+        setPage(key)
+        setContent(dataSplice[key])
+    }
+
+    return (
+        <div>
+            <span><p><strong>Publication</strong></p></span>
+            <div className="tabs is-centered">
+                <ul>
+                    {PageKey.map((element, i) => {
+                        return (
+                            <li className={i === page ? "is-active" : ""} onClick={() => handlePage({key:i})}><a>{element}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            {content.map((val)=>{
+                return <div dangerouslySetInnerHTML={{__html: val}} />
+            })}
+        </div>
+    )
+
+}
+
 const publication = ({htmlText}) => {
+
+
+    const tables = htmlText.match(/<table[^>]*>[^~]*?<\/table>/g).map((val) => {
+        return val.replaceAll(/<\/?table>/g, "").replaceAll(/<\/?tbody>/g, "")
+    })
+    return tables.map((i) => {
+            return singleTableWithTabsRender({val: i.split(/<\/?td><\/?tr><\/?tr><\/?td>|<\/?tr><\/?td>|<\/?td><\/?tr>/g).filter(e => e)})
+
+        }
+    )
 
 }
 
 
-export const htmlProcessing = ({htmlText}) => {
-    console.log("html process", htmlText)
+const HtmlProcessing = ({htmlText,libraries}) => {
+    const Html2React = libraries?.html2react.Component;
+
+    const publicationRegEx = /(<p><strong>\s*Publication\s*<\/strong>\s*<\/p>[^~]*?<figure[^>]*><table>[^~]*?<\/table><\/figure>)/g
+
+
+    const allRegEx = publicationRegEx
+
     htmlText = mediaText({htmlText: htmlText})
     htmlText = column({htmlText: htmlText})
     htmlText = title({htmlText: htmlText})
     htmlText = titleBreak({htmlText: htmlText})
-    htmlText = address({htmlText:htmlText})
-    htmlText = mail({htmlText:htmlText})
-    htmlText = tel({htmlText:htmlText})
-    htmlText = fax({htmlText:htmlText})
+    htmlText = address({htmlText: htmlText})
+    htmlText = mail({htmlText: htmlText})
+    htmlText = tel({htmlText: htmlText})
+    htmlText = fax({htmlText: htmlText})
+    // htmlText = publication({htmlText: htmlText})
     console.log("html after process", htmlText)
-    return htmlText
 
+    const splitBetweenHtmlAndComponent =  htmlText.split(allRegEx)
+    console.log("split:",splitBetweenHtmlAndComponent)
+    const Render = splitBetweenHtmlAndComponent.map((val)=> {
+       if (val.match(publicationRegEx)) {
+           return publication({htmlText:val})
+       }else{
+           return <Html2React html={val}/>
+       }
+    })
 
+    console.log("Render:",Render)
+
+    return Render
 }
+
+export default connect(HtmlProcessing)
